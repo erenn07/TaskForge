39061da6f1a1c629026ad9a4be245a0673174bec
Eren <erenkeskin07@gmail.com>
2023-12-19 21:51:10 +0300

    Fix: UserController errors resolved

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -2,13 +2,13 @@ import User from '../models/User.js';
 
 
 
-const getProfile = async(req,res)=>{
+const getProfile =async (req,res)=>{
     try {
         const user= await User.findById(req.user._id);
-        console.log(user)
-        return user; 
+        res.json(user)
       } catch (error) {
-        throw error;
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
       }
 }
 
