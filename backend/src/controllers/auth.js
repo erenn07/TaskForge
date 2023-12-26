c771182ca9eee7a1ea79a395af8ecabfb7ffc87d
MeltemAlbayrakk <77754573+MeltemAlbayrakk@users.noreply.github.com>
2023-12-26 16:57:21 +0300

    fix: auth.js logout domain updated

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -87,7 +87,7 @@ const logout = async (req, res) => {
   try {
   
     res.clearCookie('jwt',{
-      domain: 'taskforge-4yve.onrender.com', 
+      domain: 'taskforgee.netlify.app', 
       path:"/"
     }); 
     res.status(200).json({
