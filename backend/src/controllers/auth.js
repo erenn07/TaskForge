c16a24839153bda9772e76640c7e0a6560262c58
MeltemAlbayrakk <77754573+MeltemAlbayrakk@users.noreply.github.com>
2023-12-26 17:50:29 +0300

    fix: auth.js updated

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -65,6 +65,7 @@ const login = async (req, res, next) => {
 
     res.cookie('jwt', token, {
       httpOnly: true,
+      path:"/",
       maxAge: 1000 * 60 * 60 * 24,
       secure: true,
       sameSite:'none'
