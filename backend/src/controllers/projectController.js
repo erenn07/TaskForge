ac74ec9559f04a2d416783b698913f04473f427e
Eren <erenkeskin07@gmail.com>
2024-01-02 15:14:22 +0300

    Update: Update projectController.js

--- backend/src/controllers/projectController.js
+++ backend/src/controllers/projectController.js
@@ -1,9 +1,31 @@
+import Project from "../models/Project.js";
+
+const addProject =async(req,res)=>{
+    try{  
+    console.log("addproject controller")
+        const {customerId,customerProjectName,customerEmail} = req.body;
+        console.log("addproject 2",customerProjectName)
+        console.log("addproject 3",customerEmail)
+        console.log("addproject 3",customerId)
 
 
-const addProject =async()=>{
+        const isEmailValid = validateEmail(customerEmail);
+        if (!isEmailValid) {
+        return res.status(400).json({ success: false, message: 'Geçerli bir email adresi giriniz.' });
+      }
 
-}
-
+      const project = await Project.create({
+        projectName:customerProjectName,
+        customer:customerId,
+        /* tasks:[], */
+      })
+        await project.save();
+        res.status(200).json({message:'project added successfully'})
+      
+    } catch (error) {
+      
+    }
+    }
 const getProject =async ()=>{
 
 }
@@ -11,4 +33,9 @@ const getProject =async ()=>{
 const getProjectDetails =async ()=>{
 
 }
+
+const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
 export{addProject,getProject,getProjectDetails}
