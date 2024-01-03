42249175aacce85fd791a3bee81ef71b484b9e65
alinr10 <alinr10@hotmail.com>
2024-01-03 18:50:11 +0300

    add update customer

--- backend/src/controllers/customerController.js
+++ backend/src/controllers/customerController.js
@@ -1,109 +1,118 @@
 import Customer from "../models/Customer.js";
 import User from "../models/User.js";
 
-const addCustomer= async(req,res)=>{
-    try {
-      
-      const {name, surname,phone,email,projectName,userId} = req.body;
+const addCustomer = async (req, res) => {
+  try {
 
-      console.log("add customer ici ",userId)
+    const { name, surname, phone, email, projectName, userId } = req.body;
 
-      const isEmailValid = validateEmail(email);
-      if (!isEmailValid) {
-        return res.status(400).json({ success: false, message: 'Geçerli bir email adresi giriniz.' });
-      }
+    console.log("add customer ici ", userId)
 
-      if (!validatePhone(phone)) {
-        return res.status(400).json({ success: false, message: 'Telefon numarası sadece sayı içermelidir ve 10 haneli olmalıdır.' });
-      }
-
-      const customer = await Customer.create({
-        firstName:name,
-        lastName:surname,
-        phone:phone,
-        projectName:projectName,
-        email:email,
-        creatorID:userId
-      })
-        await customer.save();
-        res.status(200).json({message:'customer added successfully'})
-      
-    } catch (error) {
-      
-    }
+    const isEmailValid = validateEmail(email);
+    if (!isEmailValid) {
+      return res.status(400).json({ success: false, message: 'Geçerli bir email adresi giriniz.' });
     }
 
-    const getCustomers=async(req,res)=>{
-        try {
-
-         
-          const { userId } = req.query;
-          const customer = await Customer.find({creatorID:userId});
-         // const customers = customer.customers;
-          console.log("musteriler",customer)
-
-          res.status(200).json({customer})
-
-        } catch (error) {
-            
-        }
+    if (!validatePhone(phone)) {
+      return res.status(400).json({ success: false, message: 'Telefon numarası sadece sayı içermelidir ve 10 haneli olmalıdır.' });
     }
-    const deleteCustomer = async (req, res) => {
-      try {
-       const {id}=req.query
-    
-        const deletedCustomer = await Customer.findByIdAndDelete(id);     
-    
-        if (!deletedCustomer) {
-          console.log('No data to delete was found');
-          return res.status(404).send('No data to delete was found');
-        }
-    
-         res.status(200).send('customer deleted successfully');
-      } catch (error) {
-        console.error(error);
-        return res.status(500).send('Server error');
-      }
-    };
 
-    const updateCustomer =async (req,res)=>{
- const customerId = req.params.id;
-      const { field, updatedValue } = req.body;
-    
-      try {
-        const customer = await Customer.findById(customerId);
-        if (!customer) {
-          return res.status(404).json({ success: false, message: 'Müşteri bulunamadı' });
-        }
-    
-        customer[field] = updatedValue;
-        await customer.save();
-    
-        return res.status(200).json({ success: true, message: 'Müşteri başarıyla güncellendi' });
-      } catch (error) {
-        console.error('Müşteri güncellenirken hata oluştu:', error);
-        return res.status(500).json({ success: false, message: 'Müşteri güncellenirken bir hata oluştu' });
-      }
+    const customer = await Customer.create({
+      firstName: name,
+      lastName: surname,
+      phone: phone,
+      projectName: projectName,
+      email: email,
+      creatorID: userId
+    })
+    await customer.save();
+    res.status(200).json({ message: 'customer added successfully' })
+
+  } catch (error) {
+
+  }
+}
+
+const getCustomers = async (req, res) => {
+  try {
 
 
+    const { userId } = req.query;
+    const customer = await Customer.find({ creatorID: userId });
+    // const customers = customer.customers;
+    console.log("musteriler", customer)
 
+    res.status(200).json({ customer })
+
+  } catch (error) {
+
+  }
+}
+const deleteCustomer = async (req, res) => {
+  try {
+    const { id } = req.query
+
+    const deletedCustomer = await Customer.findByIdAndDelete(id);
+
+    if (!deletedCustomer) {
+      console.log('No data to delete was found');
+      return res.status(404).send('No data to delete was found');
     }
-    
+
+    res.status(200).send('customer deleted successfully');
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send('Server error');
+  }
+};
+
+const updateCustomer = async (req, res) => {
+  const id = req.params.id;
+  const updatedData = req.body;
+
+  console.log(updatedData, " datalar")
+
+  try {
+    const customer = await Customer.findById(id);
+    if (!customer) {
+      return res.status(404).json({ success: false, message: 'Müşteri bulunamadı' });
+    }
+
+
+    for (let field in updatedData) {
+      if (field !== 'creatorID') {
+        customer[field] = updatedData[field];
+      }
+
+      console.log(field)
+      console.log(updatedData[field])
+    }
+
+    await customer.save();
+
+    return res.status(200).json({ success: true, message: 'Müşteri başarıyla güncellendi' });
+  } catch (error) {
+    console.error('Müşteri güncellenirken hata oluştu:', error);
+    return res.status(500).json({ success: false, message: 'Müşteri güncellenirken bir hata oluştu' });
+  }
+}
 
 
 
 
 
 
-    const validateEmail = (email) => {
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return emailRegex.test(email);
-    };
 
-    const validatePhone = (phone) => {
-      const phoneRegex = /^\d{10}$/;
-      return phoneRegex.test(phone);
-    };  
-    
 
-    export{addCustomer,getCustomers,deleteCustomer,updateCustomer}
+const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+const validatePhone = (phone) => {
+  const phoneRegex = /^\d{10}$/;
+  return phoneRegex.test(phone);
+};
+
+
+export { addCustomer, getCustomers, deleteCustomer, updateCustomer }
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -19,7 +19,7 @@ instance.interceptors.response.use(
   },
   function (error) {
     if (401 === error.response.status && error.response) {
-      
+
       localStorage.clear();
       //window.location.href = "/login";
     } else {
@@ -28,166 +28,165 @@ instance.interceptors.response.use(
   }
 );
 
-export default{
-    user:{
-        async login(email,password){
-            const response = await instance.post("http://localhost:3001/auth/login",{email,password},{ withCredentials: true });
-            return response.data;
-        },
-        async register(payload){
-            const response = await instance.post("http://localhost:3001/auth/register",payload,{ withCredentials: true });
-            return response.data;
-        },
-        async getUser(token){
-          const response = await instance.get("http://localhost:3001/user/getProfile",{ 
-            withCredentials: true,
-            headers:{
-              Authorization:`Bearer ${token}`,
-            }
-           });
-          return response;
-        },
- 
-        async logout(){
-
-          try {
-         
-         localStorage.removeItem('userToken')
-            const response = await axios.get("http://localhost:3001/auth/logout", { withCredentials: true });
-            
-
-              return response
-          
-         
-         
-            }catch(error){
-            alert(error)
-          
-        }},
-
-
-
-// async checkUser(){
-  
-//     try {
-//         const response = await axios.get("http://localhost:3001/auth/checkUser", {withCredentials: true});
-
-//                   return response
-              
-//               }catch(error){
-//                 alert(error)
-//               }
-//         }
-
-
-      },
-      customer:{
-        async getCustomers(userId){
-          try {
-            const response = await axios.get("http://localhost:3001/customer/getCustomers",{
-              params: { userId },  
-              withCredentials: true
-            });
-
-              return response;
-          
-          }catch(error){
-            alert(error)
-          }
-        },
-        async addCustomer(name,surname,email,phone,projectName,userId){
-          try {
-            const response = await axios.post("http://localhost:3001/customer/addCustomer",{name,surname,email,phone,projectName,userId} ,{withCredentials: true});
-
-              return response.data;
-          
-          }catch(error){
-            alert(error)
-          }
-        },
-        async deleteCustomer(id){
-          try {
-            const response = await axios.get("http://localhost:3001/customer/deleteCustomers",{params:{id}} ,{withCredentials: true});
-
-              return response.data;
-          
-          }catch(error){
-            alert(error)
-          }
-        },
-        async updateCustomer (id, field, updatedValue) {
-          try {
-            const response = await axios.post(
-              `http://localhost:3001/customer/updateCustomer/${id}`,
-              { [field]: updatedValue },
-              { withCredentials: true }
-            );
-        
-            return response.data;
-          } catch (error) {
-            console.error('Müşteri güncellenirken hata oluştu:', error);
-            throw new Error('Müşteri güncellenirken bir hata oluştu.');
-          }
+export default {
+  user: {
+    async login(email, password) {
+      const response = await instance.post("http://localhost:3001/auth/login", { email, password }, { withCredentials: true });
+      return response.data;
+    },
+    async register(payload) {
+      const response = await instance.post("http://localhost:3001/auth/register", payload, { withCredentials: true });
+      return response.data;
+    },
+    async getUser(token) {
+      const response = await instance.get("http://localhost:3001/user/getProfile", {
+        withCredentials: true,
+        headers: {
+          Authorization: `Bearer ${token}`,
         }
-        
+      });
+      return response;
+    },
+
+    async logout() {
+
+      try {
+
+        localStorage.removeItem('userToken')
+        const response = await axios.get("http://localhost:3001/auth/logout", { withCredentials: true });
 
 
-      },
-      project:{
-        async addProject(payload){
-          try {
-            const response = await axios.post("http://localhost:3001/project/addProject",payload ,{withCredentials: true});
+        return response
 
-              return response.data;
-          
-          }catch(error){
-            alert(error)
-          }
-        },
-        async getProjects(payload){
-          try {
-            const response = await axios.get("http://localhost:3001/project/getProjects" ,{withCredentials: true});
 
-              return response.data;
-          
-          }catch(error){
-            alert(error)
-          }
-        },
-        async getProjectDetails(payload){
-          try {
-            const response = await axios.get("http://localhost:3001/project/getProjectDetails" ,{withCredentials: true});
 
-              return response.data;
-          
-          }catch(error){
-            alert(error)
-          }
-        }
-      },
-      task:{
-        async addTask(payload){
-          try {
-            const response = await axios.post("http://localhost:3001/task/addTask" ,payload,{withCredentials: true});
+      } catch (error) {
+        alert(error)
 
-              return response.data;
-          
-          }catch(error){
-            alert(error)
-          }
-        },
-        async getTask(){
-          try {
-            const response = await axios.get("http://localhost:3001/task/getTask" ,{withCredentials: true});
-
-              return response.data;
-          
-          }catch(error){
-            alert(error)
-          }
-        }
       }
-  
+    },
 
-  
+
+
+    // async checkUser(){
+
+    //     try {
+    //         const response = await axios.get("http://localhost:3001/auth/checkUser", {withCredentials: true});
+
+    //                   return response
+
+    //               }catch(error){
+    //                 alert(error)
+    //               }
+    //         }
+
+
+  },
+  customer: {
+    async getCustomers(userId) {
+      try {
+        const response = await axios.get("http://localhost:3001/customer/getCustomers", {
+          params: { userId },
+          withCredentials: true
+        });
+
+        return response;
+
+      } catch (error) {
+        alert(error)
+      }
+    },
+    async addCustomer(name, surname, email, phone, projectName, userId) {
+      try {
+        const response = await axios.post("http://localhost:3001/customer/addCustomer", { name, surname, email, phone, projectName, userId }, { withCredentials: true });
+
+        return response.data;
+
+      } catch (error) {
+        alert(error)
+      }
+    },
+    async deleteCustomer(id) {
+      try {
+        const response = await axios.get("http://localhost:3001/customer/deleteCustomers", { params: { id } }, { withCredentials: true });
+
+        return response.data;
+
+      } catch (error) {
+        alert(error)
+      }
+    },
+    async updateCustomer(updatedData) {
+      try {
+        const response = await axios.post(
+          `http://localhost:3001/customer/updateCustomer/${updatedData.id}`,
+          updatedData,
+          { withCredentials: true }
+        );
+        return response.data;
+      } catch (error) {
+        throw new Error('Müşteri güncellenirken hata oluştu: ' + error);
+      }
+    }
+
+
+
+  },
+  project: {
+    async addProject(payload) {
+      try {
+        const response = await axios.post("http://localhost:3001/project/addProject", payload, { withCredentials: true });
+
+        return response.data;
+
+      } catch (error) {
+        alert(error)
+      }
+    },
+    async getProjects(payload) {
+      try {
+        const response = await axios.get("http://localhost:3001/project/getProjects", { withCredentials: true });
+
+        return response.data;
+
+      } catch (error) {
+        alert(error)
+      }
+    },
+    async getProjectDetails(payload) {
+      try {
+        const response = await axios.get("http://localhost:3001/project/getProjectDetails", { withCredentials: true });
+
+        return response.data;
+
+      } catch (error) {
+        alert(error)
+      }
+    }
+  },
+  task: {
+    async addTask(payload) {
+      try {
+        const response = await axios.post("http://localhost:3001/task/addTask", payload, { withCredentials: true });
+
+        return response.data;
+
+      } catch (error) {
+        alert(error)
+      }
+    },
+    async getTask() {
+      try {
+        const response = await axios.get("http://localhost:3001/task/getTask", { withCredentials: true });
+
+        return response.data;
+
+      } catch (error) {
+        alert(error)
+      }
+    }
   }
+
+
+
+}
